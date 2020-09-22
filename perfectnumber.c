7d31d63c424057fdc5cecf13e0d1da4a35b860c7
rahmagehad <52938139+rahmagehad@users.noreply.github.com>
2020-09-22 18:12:07 +0200

    Add files via upload

--- perfectnumber.c
+++ perfectnumber.c
@@ -8,10 +8,9 @@ int main()
  int value;
  int x;
  int sum=0;
-
- printf("Perfect number checker \nEnter a number\n");
+void perfect(){
+printf("Perfect number checker \nEnter a number\n");
  scanf("%d", &value);
-
 
 if (value>0,value <1000){
 for(x=1;x <value ;x++){
@@ -20,16 +19,24 @@ for(x=1;x <value ;x++){
         sum=sum+x;
     }
 
-   }
+
 }
     if(sum == value)
     {
         printf ("%d",value);
-        printf (" is a perfect number \n");}
+        printf (" is a perfect number \n");
+        perfect();
+        }
   else{
       printf("%d",value);
        printf(" is not a perfect number");
+      pefect();
         }
-return 0;
+}
 }
 
+perfect();
+return 0;
+
+
+}
